884b8a97dd44630171805512d7a3bb2ec45133f7
shadowoflight <55912551+shadowoflight@users.noreply.github.com>
2020-03-27 13:52:44 +0530

    Create adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include "config.php";
+
+$result=pg_query($dbconn,"SELECT * FROM admin_login WHERE username='".$_POST["name-admin"]."';");
+
+if($result)
+{
+    echo "fetch ho rha hai";
+    $admin=pg_fetch_assoc($result);
+    $admin_name=$admin['username'];
+    $admin_password=$admin['password'];
+    $password=$_POST["password-admin"];
+    $name=$_POST["name-admin"];
+    if($name==$admin_name&& $password==$admin_password)
+    {
+        header("Location: adminpage.html");
+    }
+    else
+    {
+        header("Location: index2.html");
+    }
+}
+else
+{
+    header("Location: index2.html");
+}
+
+?>
